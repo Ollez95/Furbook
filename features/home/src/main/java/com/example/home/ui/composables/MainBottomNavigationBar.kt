82db7105fab33a2e22e705f8455068b3c34f6139
Gustavo Lorena <gustalorena@hotmail.com>
2025-03-01 16:43:29 +0100

    Fix BottomNavBar when item is selected

--- features/home/src/main/java/com/example/home/ui/composables/MainBottomNavigationBar.kt
+++ features/home/src/main/java/com/example/home/ui/composables/MainBottomNavigationBar.kt
@@ -1,6 +1,5 @@
 package com.example.home.ui.composables
 
-import android.media.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
@@ -9,23 +8,20 @@ import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.navigation.HomeNavigation
 import com.example.navigation.NavigationDestination
 import com.example.navigation.navigateToDestinationCleaningStack
-import androidx.navigation.NavDestination.Companion.hasRoute
 
 @Composable
 fun MainBottomNavigationBar(
@@ -41,7 +37,11 @@ fun MainBottomNavigationBar(
             NavigationBarItem(
                 icon = {
                     Icon(
-                        imageVector = if(isSelected) { item.iconSelected } else { item.iconNotSelected },
+                        imageVector = if (isSelected) {
+                            item.iconSelected
+                        } else {
+                            item.iconNotSelected
+                        },
                         contentDescription = item.title
                     )
                 },
@@ -49,7 +49,7 @@ fun MainBottomNavigationBar(
                 label = { Text(item.title) },
                 selected = isSelected,
                 onClick = {
-                    if(!isSelected){
+                    if (!isSelected) {
                         navController.navigateToDestinationCleaningStack(
                             navigationToClean = HomeNavigation.Main,
                             navigateToDestination = item.route,
@@ -66,24 +66,29 @@ sealed class BottomNavItem(
     val route: NavigationDestination,
     val title: String,
     val iconSelected: ImageVector,
-    val iconNotSelected: ImageVector
+    val iconNotSelected: ImageVector,
 ) {
-    data object Home: BottomNavItem(
+    data object Home : BottomNavItem(
         route = HomeNavigation.Main,
         title = "Home", iconSelected = Icons.Filled.Home,
-        iconNotSelected = Icons.Outlined.Home)
-    data object Chat: BottomNavItem(
+        iconNotSelected = Icons.Outlined.Home
+    )
+
+    data object Chat : BottomNavItem(
         route = HomeNavigation.Chat,
         title = "Chat",
         iconSelected = Icons.Filled.Call,
-        iconNotSelected = Icons.Outlined.Call)
-    data object Inbox: BottomNavItem(
+        iconNotSelected = Icons.Outlined.Call
+    )
+
+    data object Inbox : BottomNavItem(
         route = HomeNavigation.Inbox,
         title = "Inbox",
         iconSelected = Icons.Filled.Email,
-        iconNotSelected = Icons.Outlined.Email)
+        iconNotSelected = Icons.Outlined.Email
+    )
 
-    companion object{
+    companion object {
         val toList = listOf(Home, Chat, Inbox)
     }
 }
